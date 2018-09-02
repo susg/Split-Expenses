e087dcf2c7b78881b491a17ea8ae8acb03523ca5
Sushant Gupta <sushant@Sushants-MacBook-Air.local>
2018-09-02 16:57:00 +0530

    Add static files

--- split_expenses/settings.py
+++ split_expenses/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r)!zz!io4ize5htjc#qq@(ccj5)l3m&n*8+4$^+@bc&drhh4$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
